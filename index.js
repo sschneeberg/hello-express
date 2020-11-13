7c7c44dd5fa6d06752ab49569acbafeafa50357a
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-13 12:40:53 -0500

    Add routes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
     res.send('hello world');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+
 //listen for a port
 //to see this opne localhost:8000/ in chromw
 app.listen(8000);
