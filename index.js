442b1abe2a1fd0de6f0106ee7b520edc3155b6a1
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-13 12:23:45 -0500

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+//set home route /
 app.get('/', function(req, res) {
     res.send('hello world');
 });
 
 //listen for a port
+//to see this opne localhost:8000/ in chromw
 app.listen(8000);
